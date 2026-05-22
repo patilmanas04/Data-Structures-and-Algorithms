1d0ac5ad5941a3580e33e49ea39253439c39c66c
Manas Patil <patilmanas1304@gmail.com>
2026-05-22 16:58:46 +0530

    Add stack class to the utils folder in a stack.py file so that it can be reused in other code

---
+++ Linear Data Structures/utils/stack.py
@@ -0,0 +1,54 @@
+class StackOverflowException(Exception):
+    def __init__(self, message="the stack has reached it's maximum alloted capacity"):
+        super().__init__(message)
+
+
+class EmptyStackException(Exception):
+    def __init__(self, message="popping from an empty stack"):
+        super().__init__(message)
+
+
+class Stack:
+    def __init__(self, size):
+        self.__top = -1
+        self.__size = size
+        self.stack = [0] * size
+
+    def push(self, value):
+        if self.__top + 1 >= self.__size:
+            raise StackOverflowException()
+        self.__top += 1
+        self.stack[self.__top] = value
+
+    def pop(self):
+        if self.__top < 0:
+            raise EmptyStackException
+        value = self.stack[self.__top]
+        self.stack[self.__top] = 0
+        self.__top -= 1
+        return value
+
+    def peek(self):
+        if self.__top < 0:
+            raise IndexError("cannot peek from an empty array")
+        return self.stack[self.__top]
+
+    def getTop(self):
+        return self.__top
+
+    def getSize(self) -> int:
+        return self.__size
+
+    def __len__(self):
+        return self.__top + 1
+
+    def __str__(self):
+        string = "<["
+        for i in range(len(self)):
+            string += f"{self.stack[i]}"
+            string += " " if i != len(self) - 1 else ""
+        string += f"], 'Stack', size={len(self)}>"
+        return string
+
+    def isEmpty(self):
+        return True if self.__top < 0 else False
